bede296aaacd4155c2015bc4e1d6e2b9ca9f43d2
reksarka <reksarka@gmail.com>
2022-07-15 18:58:35 +0300

    Sync *.bat utils to Gist

--- scripts/unzip.bat
+++ scripts/unzip.bat
@@ -1,12 +1,9 @@
 @echo off
-
+rem  https://gist.github.com/reksar/f6112cf70d979fd7570eec04def4bb28
 setlocal
-
 set zip=%~1
 set destination=%~2
-
-rem  Requires the powershell v5+
-powershell -command ^
+rem  Requires powershell v5+
+call powershell -command ^
   "Expand-Archive '%zip%' -DestinationPath '%destination%'"
-
 endlocal
