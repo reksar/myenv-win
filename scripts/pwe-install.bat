fe0fac0e956981c49ca5e6e5c57fedc66afd62a4
reksarka <reksarka@gmail.com>
2023-10-31 02:43:18 +0200

    Adds %CD% guard for `pwe-install`
    Prevents blocking access to a dir used by %CD% when installing to.

--- scripts/pwe-install.bat
+++ scripts/pwe-install.bat
@@ -23,10 +23,16 @@ exit /b 1
 
 :INSTALL
 echo [INFO][%~n0] Installing "%app%".
+echo %CD% | findstr "%PWE_APPS%" >NUL && (
+  if not "%CD%" == "%PWE_APPS%" (
+    echo [WARN][%~n0] Going up the dir tree for safety.
+    cd "%PWE_APPS%"
+  )
+)
 call "%~dp0pwe-test\vars" || exit /b %ERRORLEVEL%
 call "%~dp0pwe-test\curl" || exit /b %ERRORLEVEL%
 call "%~dp0pwe-install\%app%" %options% || exit /b %ERRORLEVEL%
 echo [OK][%~n0] "%app%" has been installed.
-echo [INFO][%~n0] You probably need to restart the PWE.
+echo [NOTE][%~n0] Restart the PWE if needed.
 
 endlocal
